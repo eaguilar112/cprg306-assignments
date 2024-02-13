954948d1dfadfb61f84de391fcf3b8bda34687fd
eaguilar112 <erwin.aguilar123@outlook.com>
2024-02-13 01:33:52 -0700

    week 5?

--- app/page.js
+++ app/page.js
@@ -5,11 +5,18 @@ const CourseInfo = () => {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <li>
+      <p>
         <Link href="/week-2">Week 2 Assignment</Link>
+      </p>
+      <p>
         <Link href="/week-3">Week 3 Assignment</Link>
+      </p>
+      <p>
         <Link href="/week-4">Week 4 Assignment</Link>
-      </li>
+      </p>
+      <p>
+        <Link href="/week-5">Week 5 Assignment</Link>
+      </p>
     </div>
   );
 };
---
+++ app/week-5/item-list.js
@@ -0,0 +1,19 @@
+import React from "react";
+import data from "./items.json"
+
+function ItemList() {
+return (
+    <ul>
+      {items.map((item, index) => (
+        <data
+          key={index}
+          name={item.name}
+          quantity={item.quantity}
+          category={item.category}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ItemList;
