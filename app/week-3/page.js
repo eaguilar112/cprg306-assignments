bcccc1048379e84c0ddcfb9078fff65c67c7c8a0
eaguilar112 <erwin.aguilar123@outlook.com>
2024-02-13 00:43:13 -0700

    final change

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ItemList from './item-list';
 
-const Page = () => {
+function Page() {
   return (
     <main className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
