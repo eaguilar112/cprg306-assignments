b648399d776aa92df6386bd8af432839e766a63b
eaguilar112 <erwin.aguilar123@outlook.com>
2024-02-12 23:23:20 -0700

    student info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Your Name</h1>
+      <p>
+        <Link href="https://github.com/eaguilar112">
+          <a>Link to GitHub Repository</a>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
