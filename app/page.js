768e9fc214ef310ce318af50ae1478c0d65a06f8
eaguilar112 <erwin.aguilar123@outlook.com>
2024-02-12 23:41:24 -0700

    test last

--- app/page.js
+++ app/page.js
@@ -6,9 +6,7 @@ const CourseInfo = () => {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="/week-2">
-          <a>Week 2 Page</a>
-        </Link>
+        <Link href="/week-2">Week 2 Assignment</Link>
       </p>
     </div>
   );
