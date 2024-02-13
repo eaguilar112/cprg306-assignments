795d1496d7e2968321dcb03ae4ce6f59a8ecfd50
eaguilar112 <erwin.aguilar123@outlook.com>
2024-02-12 23:24:07 -0700

    name

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const StudentInfo = () => {
   return (
     <div>
-      <h1>Your Name</h1>
+      <h1>Erwin Aguilar</h1>
       <p>
         <Link href="https://github.com/eaguilar112">
           <a>Link to GitHub Repository</a>
