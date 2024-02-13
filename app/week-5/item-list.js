687dbd5ef668473577f56eea3432865f5bb0a31d
eaguilar112 <erwin.aguilar123@outlook.com>
2024-02-13 08:11:12 -0700

    buttons

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -2,18 +2,29 @@ import React, { useState} from "react";
 import Item from "./item";
 import data from "./items.json";
 
-function ItemList () {
+function SortItems () {
+  const [name, sortBy] = useState("name");
+  const [sorted, setSortBy] = useState(null);
+
+  function handleClick(category) {
+    if (sortBy === category) {
+      //
+    }
+  }
+
     return (
-        <ul>
-          {data.map((item, index) => (
-            <Item
-              key={index}
-              name={item.name}
-              quantity={item.quantity}
-              category={item.category}
-            />
-          ))}
-        </ul>
+        <div className="category buttons">
+          <button
+            type="onClick"
+            className="bg-blue-400 hover:bg-blue-500 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Name
+          </button>
+          <button
+            type="onClick"
+            className="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Category
+          </button>
+        </div>
       );
 };
 
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -5,7 +5,6 @@ function Page() {
   return (
     <main className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
-      <ItemList />
     </main>
   );
 };
