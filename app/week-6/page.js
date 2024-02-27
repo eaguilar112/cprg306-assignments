45856afaa7ea8af8aadf91b618e9bf7b1c908175
eaguilar112 <erwin.aguilar123@outlook.com>
2024-02-27 15:26:10 -0700

    test

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import ItemList from './item-list';
 import NewItem from './new-item';
 import itemsData from './items.json';
 
 function Page() {
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (newItem) => {
+    setItems(prevItems => [...prevItems, newItem]);
+  };
+
   return (
     <main className="container mx-auto px-4 py-8" style={{ backgroundColor: '#5297ff'}}>
       <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
-      <ItemList />
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items={items} />
     </main>
   );
 };
