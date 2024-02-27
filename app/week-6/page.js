de5ad5d625cbb64bda5fc63a0559ef5555797f22
eaguilar112 <erwin.aguilar123@outlook.com>
2024-02-27 15:31:00 -0700

    f

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -11,11 +11,11 @@ function Page() {
   };
 
   return (
-    <main className="container mx-auto px-4 py-8" style={{ backgroundColor: '#5297ff'}}>
+    <div className="container mx-auto px-4 py-8" style={{ backgroundColor: '#5297ff'}}>
       <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
       <NewItem onAddItem={handleAddItem} />
       <ItemList items={items} />
-    </main>
+    </div>
   );
 };
 
