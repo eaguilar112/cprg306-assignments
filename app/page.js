ee5923bd4626e977b2f5cf9fbe648e22cab04bc2
eaguilar112 <erwin.aguilar123@outlook.com>
2024-03-04 08:37:28 -0700

    bg

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const CourseInfo = () => {
   return (
-    <div>
+    <div className='bg-blue'>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
         <Link href="/week-2">Week 2 Assignment</Link>
